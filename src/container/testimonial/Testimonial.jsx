22ffde0a6879cbce0a3d82e92d12c2989d436b46
nishant <nishantthesingh@gmail.com>
2022-05-06 21:51:25 +0530

    Protfolio --v1

--- src/container/testimonial/Testimonial.jsx
+++ src/container/testimonial/Testimonial.jsx
@@ -4,8 +4,91 @@ import { AppWrap, MotionWrap } from "../../wrapper";
 import { urlFor, client } from "../../client";
 import { HiChevronLeft, HiChevronRight } from "react-icons/hi";
 import "./Testimonial.scss";
-function Testimonial() {
-  return <div>Testimonial</div>;
-}
 
-export default Testimonial;
+const Testimonial = () => {
+  const [brands, setBrands] = useState([]);
+  const [testimonials, setTestimonials] = useState([]);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const testimonialQuery = '*[_type == "testimonials"]';
+    const brandQuery = '*[_type == "brands"]';
+
+    client.fetch(testimonialQuery).then((data) => {
+      setTestimonials(data);
+    });
+
+    client.fetch(brandQuery).then((data) => {
+      setBrands(data);
+    });
+  }, []);
+
+  const currentTest = testimonials[currentIndex];
+  const handleClick = (index) => {
+    setCurrentIndex(index);
+  };
+
+  return (
+    <>
+      {testimonials.length && (
+        <>
+          <div className="app__testimonial-item app__flex">
+            <img src={urlFor(currentTest.imgurl)} alt="testimonial" />
+            <div className="app__testimonial-content">
+              <p className="p-text">{currentTest.feedback} </p>
+              <div>
+                <h4 className="bold-text">{currentTest.name}</h4>
+                <h5 className="p-text">{currentTest.company}</h5>
+              </div>
+            </div>
+          </div>
+
+          <div className="app__testimonial-btns app__flex">
+            <div
+              className="app__flex"
+              onClick={() =>
+                handleClick(
+                  currentIndex === 0
+                    ? testimonials.length - 1
+                    : currentIndex - 1
+                )
+              }
+            >
+              <HiChevronLeft />
+            </div>
+            <div
+              className="app__flex"
+              onClick={() =>
+                handleClick(
+                  currentIndex === testimonials.length - 1
+                    ? 0
+                    : currentIndex + 1
+                )
+              }
+            >
+              <HiChevronRight />
+            </div>
+          </div>
+        </>
+      )}
+
+      <div className="app__testimonial-brands app__flex">
+        {brands.map((brand) => (
+          <motion.div
+            whileInView={{ opacity: [0, 1] }}
+            transition={{ duration: 0.5, type: "tween" }}
+            key={brand._id}
+          >
+            <img src={urlFor(brand.imgUrl)} alt={brand.name} />
+          </motion.div>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default AppWrap(
+  MotionWrap(Testimonial, "app__testimonial"),
+  "testimonial",
+  "app__primarybg"
+);
--- src/container/work/Work.jsx
+++ src/container/work/Work.jsx
@@ -68,51 +68,32 @@ const Work = () => {
         className="app__work-portfolio"
       >
         {filteredWork.map((work, index) => (
-          <div className="app__work-item app_flex" key={index}>
-            <div className="app__work-img app__flex">
-              <img src={urlFor(work.imgUrl)} alt={work.name} />
-              {/* only visible on hover, implemented using motion.div */}
-              <motion.div
-                whileHover={{ opacity: [0, 1] }}
-                transition={{
-                  duration: 0.25,
-                  ease: "easeInOut",
-                  staggerChildren: 0.5,
-                }}
-                className="app__work-hover app__flex"
-              >
-                <a href={work.projectLink} target="blank" rel="noreferrer">
+          <div className="app__flex app__work-card">
+            <div className="app__work-item app_flex" key={index}>
+              <a href={work.projectLink} target="blank" rel="noreferrer">
+                <div className="app__work-img app__flex">
                   <motion.div
                     whileHover={{ scale: [1, 0.9] }}
                     whileInView={{ scale: [0, 1] }}
                     transition={{
                       duration: 0.25,
                     }}
-                    className=" app__flex"
+                    className=" app__flex app__work-github"
                   >
-                    <AiFillEye />
+                    <a href={work.codeLink} target="blank" rel="noreferrer">
+                      <AiFillGithub />
+                    </a>
                   </motion.div>
-                </a>
-                <a href={work.codeLink} target="blank" rel="noreferrer">
-                  <motion.div
-                    whileHover={{ scale: [1, 0.9] }}
-                    whileInView={{ scale: [0, 1] }}
-                    transition={{
-                      duration: 0.25,
-                    }}
-                    className=" app__flex"
-                  >
-                    <AiFillGithub />
-                  </motion.div>
-                </a>
-              </motion.div>
-            </div>
-            <div className="app__work-content app__flex">
-              <h4 className="bold-text">{work.title}</h4>
-              <p className="p-text">{work.description}</p>
-              <div className="app__work-tag app--flex">
-                <p className="p-text">{work.tags[0]}</p>
-              </div>
+                  <img src={urlFor(work.imgUrl)} alt={work.name} />
+                </div>
+                <div className="app__work-content app__flex">
+                  <h4 className="bold-text">{work.title}</h4>
+                  <p className="p-text">{work.description}</p>
+                  <div className="app__work-tag app--flex">
+                    <h5 className="p-text">{work.tags[0]}</h5>
+                  </div>
+                </div>
+              </a>
             </div>
           </div>
         ))}
