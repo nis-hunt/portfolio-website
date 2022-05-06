c1e6a27446b8226a392d415f0ae0d0d669cbc86f
nishant <nishantthesingh@gmail.com>
2022-05-06 22:05:42 +0530

    updated email

--- src/container/footer/Footer.jsx
+++ src/container/footer/Footer.jsx
@@ -48,8 +48,8 @@ const Footer = () => {
       <div className="app__footer-cards">
         <div className="app__footer-card ">
           <img src={images.email} alt="email" />
-          <a href="mailto:hello@micael.com" className="p-text">
-            hello@micael.com
+          <a href="mailto:nishantthesingh@gmail.com" className="p-text">
+            nishantthesingh@gmail.com
           </a>
         </div>
         <div className="app__footer-card">
