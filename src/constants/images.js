64a44e518999e41af1f189bc9b7d0f07e1fc802c
nishant <nishantthesingh@gmail.com>
2022-05-07 22:53:32 +0530

    added layers

--- src/constants/images.js
+++ src/constants/images.js
@@ -34,6 +34,8 @@ import bolt from "../assets/bolt.png";
 import nb from "../assets/nb.png";
 import skype from "../assets/skype.png";
 import spotify from "../assets/spotify.png";
+import layerUp from "../assets/layerUp.svg";
+import layerDown from "../assets/layerDown.svg";
 
 export default {
   email,
@@ -69,4 +71,6 @@ export default {
   nb,
   skype,
   spotify,
+  layerUp,
+  layerDown,
 };
